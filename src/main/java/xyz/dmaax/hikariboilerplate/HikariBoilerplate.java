2015035fc8baacbfb563bc015da3d6c5c309e102
dmaax <dudzcool@gmail.com>
2023-07-16 20:19:35 -0300

    feat: Added config load method

--- src/main/java/xyz/dmaax/hikariboilerplate/HikariBoilerplate.java
+++ src/main/java/xyz/dmaax/hikariboilerplate/HikariBoilerplate.java
@@ -1,14 +1,20 @@
 package xyz.dmaax.hikariboilerplate;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.dmaax.hikariboilerplate.managers.ConfigManager;
 
 public final class HikariBoilerplate extends JavaPlugin {
 
     private static HikariBoilerplate plugin;
+    private ConfigManager configManager;
+    private boolean isMySQL;
 
     @Override
     public void onEnable() {
         plugin = this;
+        configManager = new ConfigManager();
+        configManager.load();
+        isMySQL = configManager.isSql();
     }
 
     @Override
